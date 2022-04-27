9a12dca8848e1dcb6c2aeb46ab72746440e9b0ef
Neea <neearusch@gmail.com>
2022-04-26 20:04:45 -0400

    more transformations

--- parallel/jacobi-1d.c
+++ parallel/jacobi-1d.c
@@ -69,13 +69,24 @@ void kernel_jacobi_1d(int tsteps,
   int t, i;
 
 #pragma scop
-  for (t = 0; t < _PB_TSTEPS; t++)
+
+    #pragma omp parallel private(t, i)
     {
-      for (i = 1; i < _PB_N - 1; i++)
-	B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1]);
-      for (i = 1; i < _PB_N - 1; i++)
-	A[i] = 0.33333 * (B[i-1] + B[i] + B[i + 1]);
+        #pragma omp for nowait
+        for (t = 0; t < _PB_TSTEPS; t++)
+        {
+          for (i = 1; i < _PB_N - 1; i++)
+            B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1]);
+        }
+
+        #pragma omp for nowait
+        for (t = 0; t < _PB_TSTEPS; t++)
+        {
+          for (i = 1; i < _PB_N - 1; i++)
+            A[i] = 0.33333 * (B[i-1] + B[i] + B[i + 1]);
+        }
     }
+
 #pragma endscop
 
 }
