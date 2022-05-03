3093e1cc3a8da8f0e8a19827c88793e6cd663fc3
Neea <neearusch@gmail.com>
2022-05-03 15:51:36 -0400

    edit pragmas

--- fission_manual/3mm.c
+++ fission_manual/3mm.c
@@ -81,36 +81,38 @@ void kernel_3mm(int ni, int nj, int nk, int nl, int nm,
   int i, j, k;
 
 #pragma scop
-    #pragma omp parallel private(i, j)
-  {
+#pragma omp parallel private(i, j)
+{
   #pragma omp for nowait 
   /* E := A*B */
   for (i = 0; i < _PB_NI; i++)
     for (j = 0; j < _PB_NJ; j++)
       {
-	E[i][j] = SCALAR_VAL(0.0);
-	for (k = 0; k < _PB_NK; ++k)
-	  E[i][j] += A[i][k] * B[k][j];
+    	E[i][j] = SCALAR_VAL(0.0);
+	    for (k = 0; k < _PB_NK; ++k)
+    	    E[i][j] += A[i][k] * B[k][j];
       }
-  #pragma omp for nowait
+  #pragma omp for
   /* F := C*D */
   for (i = 0; i < _PB_NJ; i++)
     for (j = 0; j < _PB_NL; j++)
       {
-	F[i][j] = SCALAR_VAL(0.0);
-	for (k = 0; k < _PB_NM; ++k)
-	  F[i][j] += C[i][k] * D[k][j];
+	    F[i][j] = SCALAR_VAL(0.0);
+	    for (k = 0; k < _PB_NM; ++k)
+	        F[i][j] += C[i][k] * D[k][j];
       }
-  /* G := E*F */
-  #pragma omp for
-  for (i = 0; i < _PB_NI; i++)
-    for (j = 0; j < _PB_NL; j++)
-      {
-	G[i][j] = SCALAR_VAL(0.0);
-	for (k = 0; k < _PB_NJ; ++k)
-	  G[i][j] += E[i][k] * F[k][j];
-      }
+}
+/* G := E*F */
+#pragma omp parallel for
+for (i = 0; i < _PB_NI; i++)
+  for (j = 0; j < _PB_NL; j++)
+  {
+    G[i][j] = SCALAR_VAL(0.0);
+    for (k = 0; k < _PB_NJ; ++k)
+    G[i][j] += E[i][k] * F[k][j];
   }
+
+
 #pragma endscop
 
 }
