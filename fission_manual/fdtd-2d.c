1ac83d8b16079da2fc7fe8d856f30e5982132654
Neea <neearusch@gmail.com>
2022-05-03 08:13:56 -0400

    -jacobi 1,2

--- fission_manual/fdtd-2d.c
+++ fission_manual/fdtd-2d.c
@@ -121,14 +121,14 @@ void kernel_fdtd_2d(int tmax,
     }
 }
 
-
-	for(t = 0; t < _PB_TMAX; t++)
-    {
-      for (i = 0; i < _PB_NX - 1; i++)
-	    for (j = 0; j < _PB_NY - 1; j++)
-	        hz[i][j] = hz[i][j] - SCALAR_VAL(0.7)*  (ex[i][j+1] - ex[i][j] +
-				       ey[i+1][j] - ey[i][j]);
-    }
+#pragma omp parallel for
+for(t = 0; t < _PB_TMAX; t++)
+{
+  for (i = 0; i < _PB_NX - 1; i++)
+    for (j = 0; j < _PB_NY - 1; j++)
+        hz[i][j] = hz[i][j] - SCALAR_VAL(0.7)*  (ex[i][j+1] - ex[i][j] +
+                   ey[i+1][j] - ey[i][j]);
+}
 
 #pragma endscop
 }
