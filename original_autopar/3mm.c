0b6c2b45c51444cbfef89206a8e78ff7a51afec5
Clément Aubert <caubert@augusta.edu>
2022-05-03 20:29:13 -0400

    Restoring examples.

--- original_autopar/3mm.c
+++ original_autopar/3mm.c
@@ -3,8 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <polybench.h>
-#include "3mm.h"
-#include <omp.h>
+#include <3mm.h>
 /**
 * This version is stamped on May 10, 2016
 *
@@ -18,25 +17,31 @@
 /*Include polybench common header.*/
 /*Include benchmark-specific header.*/
 /*Array initialization.*/
-static void init_array(int ni, int nj, int nk, int nl, int nm, double A[800][1000], double B[1000][900], double C[900][1200], double D[1200][1100])
-{
+static void init_array(int ni, int nj, int nk, int nl, int nm, double A[800][1000], double B[1000][900], double C[900][1200], double D[1200][1100]) {
    int i, j;
-   for (i = 0; i < ni; i++) for (j = 0; j < nk; j++) A[i][j] = (double) ((i * j + 1) % ni) / (5 * ni);
-   for (i = 0; i < nk; i++) for (j = 0; j < nj; j++) B[i][j] = (double) ((i * (j + 1) + 2) % nj) / (5 * nj);
-   for (i = 0; i < nj; i++) for (j = 0; j < nm; j++) C[i][j] = (double) (i * (j + 3) % nl) / (5 * nl);
-   for (i = 0; i < nm; i++) for (j = 0; j < nl; j++) D[i][j] = (double) ((i * (j + 2) + 2) % nk) / (5 * nk);
+   for(i = 0; i < ni; i++)
+      for(j = 0; j < nk; j++)
+         A[i][j] = (double) ((i * j + 1) % ni) / (5 * ni);
+   for(i = 0; i < nk; i++)
+      for(j = 0; j < nj; j++)
+         B[i][j] = (double) ((i * (j + 1) + 2) % nj) / (5 * nj);
+   for(i = 0; i < nj; i++)
+      for(j = 0; j < nm; j++)
+         C[i][j] = (double) (i * (j + 3) % nl) / (5 * nl);
+   for(i = 0; i < nm; i++)
+      for(j = 0; j < nl; j++)
+         D[i][j] = (double) ((i * (j + 2) + 2) % nk) / (5 * nk);
 }
 
 /*DCE code. Must scan the entire live-out data.
 Can be used also to check the correctness of the output.*/
-static void print_array(int ni, int nl, double G[800][1100])
-{
+static void print_array(int ni, int nl, double G[800][1100]) {
    int i, j;
    fprintf(stderr, "==BEGIN DUMP_ARRAYS==\n");
    fprintf(stderr, "begin dump: %s", "G");
-   for (i = 0; i < ni; i++) for (j = 0; j < nl; j++)
-      {
-         if ((i * ni + j) % 20 == 0) fprintf(stderr, "\n");
+   for(i = 0; i < ni; i++)
+      for(j = 0; j < nl; j++) {
+         if((i * ni + j) % 20 == 0) fprintf(stderr, "\n");
          fprintf(stderr, "%0.2lf ", G[i][j]);
       }
    fprintf(stderr, "\nend   dump: %s\n", "G");
@@ -45,54 +50,54 @@ static void print_array(int ni, int nl, double G[800][1100])
 
 /*Main computational kernel. The whole function will be timed,
 including the call and return.*/
-static void kernel_3mm(int ni, int nj, int nk, int nl, int nm, double E[800][900], double A[800][1000], double B[1000][900], double F[900][1100], double C[900][1200], double D[1200][1100], double G[800][1100])
-{
+static void kernel_3mm(int ni, int nj, int nk, int nl, int nm, double E[800][900], double A[800][1000], double B[1000][900], double F[900][1100], double C[900][1200], double D[1200][1100], double G[800][1100]) {
    int i, j, k;
-   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(ni, nj, nk)
-   for (i = 0; i < ni; i++)
-   {
-      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nj, i, nk)
-      for (j = 0; j < nj; j++)
-      {
+   #pragma scop
+   /*E := A*B*/
+   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(ni, nj, nk, A, B)
+   for(i = 0; i < ni; i++) {
+      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nj, i, nk, A, B)
+      for(j = 0; j < nj; j++) {
          E[i][j] = 0.0;
-         // #pragma omp parallel for default(shared) private(k) firstprivate(nk, i, j) reduction(+ : E[i][j])
-         for (k = 0; k < nk; ++k) E[i][j] += A[i][k] * B[k][j];
+         // #pragma omp parallel for default(shared) private(k) firstprivate(nk, i, j, A, B) reduction(+ : E[i][j])
+         for(k = 0; k < nk; ++k)
+            E[i][j] += A[i][k] * B[k][j];
       }
    }
-   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(nj, nl, nm)
-   for (i = 0; i < nj; i++)
-   {
-      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nl, i, nm)
-      for (j = 0; j < nl; j++)
-      {
+   /*F := C*D*/
+   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(nj, nl, nm, C, D)
+   for(i = 0; i < nj; i++) {
+      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nl, i, nm, C, D)
+      for(j = 0; j < nl; j++) {
          F[i][j] = 0.0;
-         // #pragma omp parallel for default(shared) private(k) firstprivate(nm, i, j) reduction(+ : F[i][j])
-         for (k = 0; k < nm; ++k) F[i][j] += C[i][k] * D[k][j];
+         // #pragma omp parallel for default(shared) private(k) firstprivate(nm, i, j, C, D) reduction(+ : F[i][j])
+         for(k = 0; k < nm; ++k)
+            F[i][j] += C[i][k] * D[k][j];
       }
    }
-   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(ni, nl, nj)
-   for (i = 0; i < ni; i++)
-   {
-      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nl, i, nj)
-      for (j = 0; j < nl; j++)
-      {
+   /*G := E*F*/
+   #pragma omp parallel for default(shared) private(i, j, k) firstprivate(ni, nl, nj, E, F)
+   for(i = 0; i < ni; i++) {
+      // #pragma omp parallel for default(shared) private(j, k) firstprivate(nl, i, nj, E, F)
+      for(j = 0; j < nl; j++) {
          G[i][j] = 0.0;
-         // #pragma omp parallel for default(shared) private(k) firstprivate(nj, i, j) reduction(+ : G[i][j])
-         for (k = 0; k < nj; ++k) G[i][j] += E[i][k] * F[k][j];
+         // #pragma omp parallel for default(shared) private(k) firstprivate(nj, i, j, E, F) reduction(+ : G[i][j])
+         for(k = 0; k < nj; ++k)
+            G[i][j] += E[i][k] * F[k][j];
       }
    }
+   #pragma endscop
 }
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
    /*Retrieve problem size.*/
    int ni = 800;
    int nj = 900;
    int nk = 1000;
    int nl = 1100;
    int nm = 1200;
-   double (*E)[800][900];
    /*Variable declaration/allocation.*/
+   double (*E)[800][900];
    E = (double (*)[800][900]) polybench_alloc_data((800 + 0) * (900 + 0), sizeof(double));
    ;
    double (*A)[800][1000];
@@ -124,9 +129,9 @@ int main(int argc, char ** argv)
    ;
    /*Prevent dead-code elimination. All live-out data must be printed
    by the function call in argument.*/
-   print_array(ni, nl, *G);
-   free((void *) E);
+   if(argc > 42 && !strcmp(argv[0], "")) print_array(ni, nl, *G);
    /*Be clean.*/
+   free((void *) E);
    ;
    free((void *) A);
    ;
@@ -140,6 +145,6 @@ int main(int argc, char ** argv)
    ;
    free((void *) G);
    ;
-
+   
    return 0;
 }
