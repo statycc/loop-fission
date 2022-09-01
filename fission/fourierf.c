7f4dfb50876f3ee0615e8f89918d5a0256bed425
Neea <neearusch@gmail.com>
2022-09-01 14:59:03 -0400

    copy files

---
+++ fission/fourierf.c
@@ -0,0 +1,216 @@
+/**
+ * This benchmark created on August 29, 2022.
+ *
+ * Inverse Fourier transforms (fft_float) method from MiBench Version 1.0.
+ * telecomm/FFT/fourierf.c, LOC 39-146.
+ *
+ * Web address: https://vhosts.eecs.umich.edu/mibench
+ */
+/* fourierf.c */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+/* Include polybench common header. */
+#include <polybench.h>
+
+/* Include benchmark-specific header. */
+/* Default data type is double, default size is N=1024. */
+#include "fourierf.h"
+
+
+/* Array initialization. */
+static
+void init_array(int m, int w,
+        DATA_TYPE POLYBENCH_1D(RealIn,M,m),
+        DATA_TYPE POLYBENCH_1D(RealOut,M,m),
+        DATA_TYPE POLYBENCH_1D(ImagIn,M,m),
+        DATA_TYPE POLYBENCH_1D(ImagOut,M,m),
+        DATA_TYPE POLYBENCH_1D(coeff,W,w),
+        DATA_TYPE POLYBENCH_1D(amp,W,w))
+{
+    int i, j, nb;
+    srand(1);
+
+    for ( i=0; ; i++ ){
+        if ( m & (1 << i) ) { nb = i; break; }
+    }
+    for (i = 0; i < w; i++) {
+        coeff[i] = rand() % 1000;
+        amp[i] = rand() % 1000;
+    }
+    for (i = 0; i < m; i++) {
+        RealIn[i] = 0;
+        for (j = 0; j < w; j++) {
+            /* randomly select sin or cos */
+            if (rand() % 2) {
+                RealIn[i] += coeff[j] * cos(amp[j] * i);
+            } else {
+                RealIn[i] += coeff[j] * sin(amp[j] * i);
+            }
+            ImagIn[i] = 0;
+        }
+    }
+    for ( i=0; i < m; i++ )
+    {
+        unsigned index=i; // reverse bits
+        for ( unsigned ii=j=0; ii < nb; ii++ )
+        {
+            j = (j << 1) | (index & 1);
+            index >>= 1;
+        }
+        RealOut[j] = RealIn[i];
+        ImagOut[j] = (ImagIn == NULL) ? 0.0f : ImagIn[i];
+    }
+}
+
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static
+void print_array(int m,
+       DATA_TYPE POLYBENCH_1D(RealOut,M,m),
+       DATA_TYPE POLYBENCH_1D(ImagOut,M,m))
+{
+    int i;
+
+    POLYBENCH_DUMP_START;
+    POLYBENCH_DUMP_BEGIN("RealOut");
+    POLYBENCH_DUMP_BEGIN("ImagOut");
+    for (i = 0; i < m; i++) {
+        fprintf (stderr, DATA_PRINTF_MODIFIER, RealOut[i]);
+        fprintf (stderr, DATA_PRINTF_MODIFIER, ImagOut[i]);
+        if (i % 20 == 0) fprintf (stderr, "\n");
+    }
+    POLYBENCH_DUMP_END("ImagOut");
+    POLYBENCH_DUMP_END("RealOut");
+    POLYBENCH_DUMP_FINISH;
+}
+
+
+/* Main computational kernel. The whole function will be timed,
+   including the call and return. */
+static
+void kernel_fourierf(int m,
+        DATA_TYPE POLYBENCH_1D(RealIn,M,m),
+        DATA_TYPE POLYBENCH_1D(RealOut,M,m),
+        DATA_TYPE POLYBENCH_1D(ImagIn,M,m),
+        DATA_TYPE POLYBENCH_1D(ImagOut,M,m))
+{
+    DATA_TYPE denom = (DATA_TYPE)m;
+    double angle_numerator = -2.0 * 3.14159265358979323846; // inverse transform
+    unsigned i, j, k, n, BlockSize, BlockEnd;
+    double tr, ti; // temp real, temp imaginary
+
+    BlockEnd = 1;
+
+#pragma scop
+
+    for ( BlockSize = 2; BlockSize <= m; BlockSize <<= 1 )
+    {
+        double delta_angle = angle_numerator / (double)BlockSize;
+        double sm2 = sin ( -2 * delta_angle );
+        double sm1 = sin ( -delta_angle );
+        double cm2 = cos ( -2 * delta_angle );
+        double cm1 = cos ( -delta_angle );
+        double w = 2 * cm1;
+        double ar[3], ai[3];
+
+        for ( i=0; i < m; i += BlockSize )
+        {
+            ar[2] = cm2;
+            ar[1] = cm1;
+
+            ai[2] = sm2;
+            ai[1] = sm1;
+
+            for ( j=i, n=0; n < BlockEnd; j++, n++ )
+            {
+                ar[0] = w*ar[1] - ar[2];
+                ar[2] = ar[1];
+                ar[1] = ar[0];
+
+                ai[0] = w*ai[1] - ai[2];
+                ai[2] = ai[1];
+                ai[1] = ai[0];
+
+                k = j + BlockEnd;
+                tr = ar[0]*RealOut[k] - ai[0]*ImagOut[k];
+                ti = ar[0]*ImagOut[k] + ai[0]*RealOut[k];
+
+                RealOut[k] = (DATA_TYPE)(RealOut[j] - tr);
+                ImagOut[k] = (DATA_TYPE)(ImagOut[j] - ti);
+
+                RealOut[j] += (DATA_TYPE)(tr);
+                ImagOut[j] += (DATA_TYPE)(ti);
+            }
+        }
+
+        BlockEnd = BlockSize;
+    }
+
+    // normalize if inverse transform
+    for ( i=0; i < m; i++ )
+    {
+        RealOut[i] /= (DATA_TYPE)(denom);
+        ImagOut[i] /= (DATA_TYPE)(denom);
+    }
+
+#pragma endscop
+
+}
+
+
+int main(int argc, char** argv)
+{
+  /* Retrieve problem size. */
+  int m = M;
+  int w = W;
+
+  /* Variable declaration/allocation. */
+  POLYBENCH_1D_ARRAY_DECL(RealIn,DATA_TYPE,M,m);
+  POLYBENCH_1D_ARRAY_DECL(RealOut,DATA_TYPE,M,m);
+  POLYBENCH_1D_ARRAY_DECL(ImagIn,DATA_TYPE,M,m);
+  POLYBENCH_1D_ARRAY_DECL(ImagOut,DATA_TYPE,M,m);
+  POLYBENCH_1D_ARRAY_DECL(coeff,DATA_TYPE,W,w);
+  POLYBENCH_1D_ARRAY_DECL(amp,DATA_TYPE,W,w);
+
+  /* Initialize array(s). */
+  init_array (m, w,
+              POLYBENCH_ARRAY(RealIn),
+              POLYBENCH_ARRAY(RealOut),
+              POLYBENCH_ARRAY(ImagIn),
+              POLYBENCH_ARRAY(ImagOut),
+              POLYBENCH_ARRAY(coeff),
+              POLYBENCH_ARRAY(amp));
+
+  /* Start timer. */
+  polybench_start_instruments;
+
+  /* Run kernel. */
+  kernel_fourierf (m,
+                   POLYBENCH_ARRAY(RealIn),
+                   POLYBENCH_ARRAY(RealOut),
+                   POLYBENCH_ARRAY(ImagIn),
+                   POLYBENCH_ARRAY(ImagOut));
+
+  /* Stop and print timer. */
+  polybench_stop_instruments;
+  polybench_print_instruments;
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+   polybench_prevent_dce(print_array(m, POLYBENCH_ARRAY(RealOut), POLYBENCH_ARRAY(ImagOut)));
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(RealIn);
+  POLYBENCH_FREE_ARRAY(RealOut);
+  POLYBENCH_FREE_ARRAY(ImagIn);
+  POLYBENCH_FREE_ARRAY(ImagOut);
+  POLYBENCH_FREE_ARRAY(coeff);
+  POLYBENCH_FREE_ARRAY(amp);
+
+  return 0;
+}
---
+++ fission/remap.c
@@ -0,0 +1,277 @@
+/**
+ * This benchmark created on August 31, 2022.
+ *
+ * The remap procedure from NAS UA benchmark.
+ * UA = Unstructured Adaptive mesh, dynamic and irregular memory access.
+ *
+ * Description of remap:
+ * After a refinement, map the solution from the parent (x) to
+ * the eight children. y is the solution on the first child
+ * (front-bottom-left) and y1 is the solution on the next 7 children.
+ *
+ * Web address: https://www.nas.nasa.gov/software/npb.html
+ */
+/* remap.c */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+/* Include polybench common header. */
+#include <polybench.h>
+
+/* Include benchmark-specific header. */
+/* Default data type is double, default size is N=1024. */
+#include "remap.h"
+
+/* these dimensions are fixed for all problem sizes */
+#define YONE_SZ 2
+#define YTWO_SZ 4
+#define Y1_SIZE 7
+
+/* Array initialization. */
+static
+void init_array(int lx,
+        DATA_TYPE POLYBENCH_4D(YONE,  YONE_SZ,LX,LX,LX,YONE_SZ,lx,lx,lx),
+        DATA_TYPE POLYBENCH_4D(YTWO,  YTWO_SZ,LX,LX,LX,YTWO_SZ,lx,lx,lx),
+        DATA_TYPE POLYBENCH_3D(X,     LX,LX,LX,lx,lx,lx),
+        DATA_TYPE POLYBENCH_3D(Y,     LX,LX,LX,lx,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixmc1, LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixmc2, LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixtmc1,LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixtmc2,LX,LX,lx,lx))
+{
+    int i, j, k, h;
+
+    for (i = 0; i < lx; i++)
+        ixtmc1[0][i] = ixtmc1[lx-1][i] = 0.0;
+    ixtmc1[0][0] = 1.0;
+    ixtmc1[lx-1][lx/2] = 1.0;
+
+    for(i = 1; i < ((lx-2)/3); i += 3){
+        for(j = 0; j < lx/5; j += 5){
+            ixtmc1[1+i][(j+0) % lx] = j + i *  0.3385078435248143;
+            ixtmc1[1+i][(j+1) % lx] = j + i *  0.7898516348912331;
+            ixtmc1[1+i][(j+2) % lx] = j + i * -0.1884018684471238;
+            ixtmc1[1+i][(j+3) % lx] = j + i *  9.202967302175333e-02;
+            ixtmc1[1+i][(j+4) % lx] = j + i * -3.198728299067715e-02;
+        }
+        for(j = 0; j < lx/5; j += 5){
+            ixtmc1[2+i][(j+0) % lx] = j + i * -0.1171875;
+            ixtmc1[2+i][(j+1) % lx] = j + i *  0.8840317166357952;
+            ixtmc1[2+i][(j+2) % lx] = j + i *  0.3125;
+            ixtmc1[2+i][(j+3) % lx] = j + i * -0.118406716635795;
+            ixtmc1[2+i][(j+4) % lx] = j + i *  0.0390625;
+        }
+        for(j = 0; j < lx/5; j += 5){
+            ixtmc1[3+i][(j+0) % lx] = j + i * -7.065070066767144e-02;
+            ixtmc1[3+i][(j+1) % lx] = j + i *  0.2829703269782467;
+            ixtmc1[3+i][(j+2) % lx] = j + i *  0.902687582732838;
+            ixtmc1[3+i][(j+3) % lx] = j + i * -0.1648516348912333;
+            ixtmc1[3+i][(j+4) % lx] = j + i *  4.984442584781999e-02;
+        }
+    }
+    for (j = 0; j < lx; j++)
+        for (i = 0; i < lx; i++)
+            ixtmc2[j][i] = ixtmc1[lx - 1 - j][lx - 1 - i];
+
+    for (i = 0; i < lx; i++)
+        for (j = 0; j < lx; j++)
+            ixmc1[j][i] = ixtmc1[i][j];
+
+    for (i = 0; i < lx; i++)
+        for (j = 0; j < lx; j++)
+            ixmc2[j][i] = ixtmc2[i][j];
+
+    for (i = 0; i < lx; i++)
+        for (j = 0; j < lx; j++)
+            for (k = 0; k < lx; k++)
+                X[i][j][k] = sin(i+j+k);
+
+    for (i = 0; i < lx; i++)
+        for (j = 0; j < lx; j++)
+            for (k = 0; k < lx; k++)
+                Y[i][j][k] = cos(i+j+k);
+
+    for (i = 0; i < YONE_SZ; i++)
+        for (j = 0; j < lx; j++)
+            for (k = 0; k < lx; k++)
+                for (h = 0; h < lx; h++)
+                    YONE[i][j][k][h] = 0;
+
+    for (i = 0; i < YTWO_SZ; i++)
+        for (j = 0; j < lx; j++)
+            for (k = 0; k < lx; k++)
+                for (h = 0; h < lx; h++)
+                    YTWO[i][j][k][h] = 0;
+}
+
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static
+void print_array(int lx, DATA_TYPE POLYBENCH_4D(Y1,Y1_SIZE,LX,LX,LX,Y1_SIZE,lx,lx,lx))
+{
+  int i, j, k, l;
+
+  for (i = 0; i < Y1_SIZE; i++)
+    for (j = 0; j < lx; j++)
+      for (k = 0; k < lx; k++)
+        for (l = 0; l < lx; l++) {
+          fprintf (stderr, DATA_PRINTF_MODIFIER, Y1[i][j][k][l]);
+          if (i % 20 == 0) fprintf (stderr, "\n");
+        }
+  fprintf (stderr, "\n");
+}
+
+
+/* Main computational kernel. The whole function will be timed,
+   including the call and return. */
+static
+void kernel_remap(int lx,
+        DATA_TYPE POLYBENCH_4D(YONE,  YONE_SZ,LX,LX,LX,YONE_SZ,lx,lx,lx),
+        DATA_TYPE POLYBENCH_4D(YTWO,  YTWO_SZ,LX,LX,LX,YTWO_SZ,lx,lx,lx),
+        DATA_TYPE POLYBENCH_4D(Y1,    Y1_SIZE,LX,LX,LX,Y1_SIZE,lx,lx,lx),
+        DATA_TYPE POLYBENCH_3D(X,     LX,LX,LX,lx,lx,lx),
+        DATA_TYPE POLYBENCH_3D(Y,     LX,LX,LX,lx,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixmc1, LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixmc2, LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixtmc1,LX,LX,lx,lx),
+        DATA_TYPE POLYBENCH_2D(ixtmc2,LX,LX,lx,lx))
+{
+   int i, iz, ii, jj, kk;;
+
+#pragma scop
+
+    for (i = 0; i < _PB_LX; i++)
+    {
+        for (kk = 0; kk < _PB_LX; kk++)
+        {
+            for (jj = 0; jj < _PB_LX; jj++)
+            {
+                for (ii = 0; ii < _PB_LX; ii++)
+                {
+                    YONE[0][i][jj][ii] = YONE[0][i][jj][ii] + ixmc1[kk][ii] * X[i][jj][kk];
+                    YONE[1][i][jj][ii] = YONE[1][i][jj][ii] + ixmc2[kk][ii] * X[i][jj][kk];
+                }
+            }
+        }
+
+        for (kk = 0; kk < _PB_LX; kk++)
+        {
+            for (jj = 0; jj < _PB_LX; jj++)
+            {
+                for (ii = 0; ii < _PB_LX; ii++)
+                {
+                    YTWO[0][jj][i][ii] = YTWO[0][jj][i][ii] +
+                                         YONE[0][i][kk][ii] * ixtmc1[jj][kk];
+                    YTWO[1][jj][i][ii] = YTWO[1][jj][i][ii] +
+                                         YONE[0][i][kk][ii] * ixtmc2[jj][kk];
+                    YTWO[2][jj][i][ii] = YTWO[2][jj][i][ii] +
+                                         YONE[1][i][kk][ii] * ixtmc1[jj][kk];
+                    YTWO[3][jj][i][ii] = YTWO[3][jj][i][ii] +
+                                         YONE[1][i][kk][ii] * ixtmc2[jj][kk];
+                }
+            }
+        }
+    }
+
+    for (iz = 0; iz < _PB_LX; iz++)
+    {
+        for (kk = 0; kk < _PB_LX; kk++)
+        {
+            for (jj = 0; jj < _PB_LX; jj++)
+            {
+                for (ii = 0; ii < _PB_LX; ii++)
+                {
+                    Y[jj][iz][ii] = Y[jj][iz][ii] +
+                                    YTWO[0][iz][kk][ii] * ixtmc1[jj][kk];
+                    Y1[0][jj][iz][ii] = Y1[0][jj][iz][ii] +
+                                        YTWO[2][iz][kk][ii] * ixtmc1[jj][kk];
+                    Y1[1][jj][iz][ii] = Y1[1][jj][iz][ii] +
+                                        YTWO[1][iz][kk][ii] * ixtmc1[jj][kk];
+                    Y1[2][jj][iz][ii] = Y1[2][jj][iz][ii] +
+                                        YTWO[3][iz][kk][ii] * ixtmc1[jj][kk];
+                    Y1[3][jj][iz][ii] = Y1[3][jj][iz][ii] +
+                                        YTWO[0][iz][kk][ii] * ixtmc2[jj][kk];
+                    Y1[4][jj][iz][ii] = Y1[4][jj][iz][ii] +
+                                        YTWO[2][iz][kk][ii] * ixtmc2[jj][kk];
+                    Y1[5][jj][iz][ii] = Y1[5][jj][iz][ii] +
+                                        YTWO[1][iz][kk][ii] * ixtmc2[jj][kk];
+                    Y1[6][jj][iz][ii] = Y1[6][jj][iz][ii] +
+                                        YTWO[3][iz][kk][ii] * ixtmc2[jj][kk];
+                }
+            }
+        }
+    }
+
+#pragma endscop
+
+}
+
+
+int main(int argc, char** argv)
+{
+  /* Retrieve problem size. */
+  int lx = LX;
+
+  /* Variable declaration/allocation. */
+  POLYBENCH_4D_ARRAY_DECL(YONE,DATA_TYPE,YONE_SZ,LX,LX,LX,YONE_SZ,lx,lx,lx);
+  POLYBENCH_4D_ARRAY_DECL(YTWO,DATA_TYPE,YTWO_SZ,LX,LX,LX,YTWO_SZ,lx,lx,lx);
+  POLYBENCH_4D_ARRAY_DECL(Y1, DATA_TYPE,Y1_SIZE,LX,LX,LX,Y1_SIZE,lx,lx,lx);
+  POLYBENCH_3D_ARRAY_DECL(Y,DATA_TYPE,LX,LX,LX,lx,lx,lx);
+  POLYBENCH_3D_ARRAY_DECL(X,DATA_TYPE,LX,LX,LX,lx,lx,lx);
+  POLYBENCH_2D_ARRAY_DECL(ixmc1,DATA_TYPE,LX,LX,lx,lx);
+  POLYBENCH_2D_ARRAY_DECL(ixmc2,DATA_TYPE,LX,LX,lx,lx);
+  POLYBENCH_2D_ARRAY_DECL(ixtmc1,DATA_TYPE,LX,LX,lx,lx);
+  POLYBENCH_2D_ARRAY_DECL(ixtmc2,DATA_TYPE,LX,LX,lx,lx);
+
+  /* Initialize array(s). */
+  init_array (lx,
+              POLYBENCH_ARRAY(YONE),
+              POLYBENCH_ARRAY(YTWO),
+              POLYBENCH_ARRAY(X),
+              POLYBENCH_ARRAY(Y),
+              POLYBENCH_ARRAY(ixmc1),
+              POLYBENCH_ARRAY(ixmc2),
+              POLYBENCH_ARRAY(ixtmc1),
+              POLYBENCH_ARRAY(ixtmc2));
+
+  /* Start timer. */
+  polybench_start_instruments;
+
+  /* Run kernel. */
+  kernel_remap (lx,
+                  POLYBENCH_ARRAY(YONE),
+                  POLYBENCH_ARRAY(YTWO),
+                  POLYBENCH_ARRAY(Y1),
+                  POLYBENCH_ARRAY(X),
+                  POLYBENCH_ARRAY(Y),
+                  POLYBENCH_ARRAY(ixmc1),
+                  POLYBENCH_ARRAY(ixmc2),
+                  POLYBENCH_ARRAY(ixtmc1),
+                  POLYBENCH_ARRAY(ixtmc2));
+
+  /* Stop and print timer. */
+  polybench_stop_instruments;
+  polybench_print_instruments;
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  polybench_prevent_dce(print_array(lx,  POLYBENCH_ARRAY(Y1)));
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(YONE);
+  POLYBENCH_FREE_ARRAY(YTWO);
+  POLYBENCH_FREE_ARRAY(Y1);
+  POLYBENCH_FREE_ARRAY(Y);
+  POLYBENCH_FREE_ARRAY(X);
+  POLYBENCH_FREE_ARRAY(ixmc1);
+  POLYBENCH_FREE_ARRAY(ixmc2);
+  POLYBENCH_FREE_ARRAY(ixtmc1);
+  POLYBENCH_FREE_ARRAY(ixtmc2);
+
+  return 0;
+}
