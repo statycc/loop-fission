0e6576d201b04ce50247a92e5bef253b75ae1493
Neea <neearusch@gmail.com>
2022-09-02 09:33:48 -0400

    private vars

--- fission/gemm.c
+++ fission/gemm.c
@@ -87,7 +87,7 @@ void kernel_gemm(int ni, int nj, int nk,
 //C is NIxNJ
 #pragma scop
 
-  #pragma omp parallel for
+  #pragma omp parallel for private(i,j,k)
   for (i = 0; i < _PB_NI; i++) {
     for (j = 0; j < _PB_NJ; j++)
 	C[i][j] *= beta;
